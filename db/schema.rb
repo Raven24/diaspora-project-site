454d444847b60bb421f86d8fd9be3be2f7711639
Dennis Schubert <software@dsx.cc>
2011-09-30 20:25:43 +0200

    Hello, i18n!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110825222750) do
+ActiveRecord::Schema.define(:version => 20110825221952) do
 
   create_table "contributors", :force => true do |t|
     t.string   "name"
